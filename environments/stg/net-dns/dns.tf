308c8cd5179495fff7c28bc567db648ea9494a94
Yar N <74919751+yaroslavopsguru@users.noreply.github.com>
2023-05-13 21:45:17 -0700

    Update dns.tf
    Signed-off-by: Yar N. <74919751+yaroslavopsguru@users.noreply.github.com>

--- environments/stg/net-dns/dns.tf
+++ environments/stg/net-dns/dns.tf
@@ -1,8 +1,8 @@
 module "cloud-dns" {
   source  = "terraform-google-modules/cloud-dns/google"
   version = "4.1.0"
-  domain = "stg.keywordinsights.tech."
-  name = "stg-keywordinsights-tech"
+  domain = "cloune.one."
+  name = "cloudon-one"
   type = "public"
 
   project_id = var.project_id
