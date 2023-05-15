8961fb8352f19e6b1e73553c4b372e05a286d17e
Yar N <74919751+yaroslavopsguru@users.noreply.github.com>
2023-05-14 21:55:29 -0700

    Update dns.tf
    Signed-off-by: Yar N. <74919751+yaroslavopsguru@users.noreply.github.com>

--- environments/prod/usa/net-dns/dns.tf
+++ environments/prod/usa/net-dns/dns.tf
@@ -1,9 +1,9 @@
 module "cloud-dns" {
   source  = "terraform-google-modules/cloud-dns/google"
   version = "4.1.0"
-  domain = "prod.keywordinsights.tech."
-  name = "prod-keywordinsights-tech"
-  type = "public"
+  domain = ""
+  name = ""
+  type = ""
 
   project_id = var.project_id
   recordsets = var.recordsets
